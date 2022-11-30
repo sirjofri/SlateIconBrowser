102cd0134f0cefa44e8e738a778c163f2154d79f
sirjofri <sirjofri@sirjofri.de>
2022-11-30 11:21:29 +0100

    adds generic build script and .zip files to .gitignore

---
+++ build.bat
@@ -0,0 +1,15 @@
+setlocal
+
+md %temp%\SlateIconBrowser
+for %%a in (5.0 5.1) do call :build %%a
+rd /s /q %temp%\SlateIconBrowser
+goto :eof
+
+:build
+md %temp%\SlateIconBrowser\%1
+for /f "tokens=2* skip=2" %%a in ('reg query "HKLM\SOFTWARE\EpicGames\Unreal Engine\%1" /v "InstalledDirectory"') do set epic=%%b
+call "%epic%\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin="%CD%\SlateIconBrowser.uplugin" -Package="%temp%\SlateIconBrowser\%1" -Rocket
+rd /s /q %temp%\SlateIconBrowser\%1\Binaries
+rd /s /q %temp%\SlateIconBrowser\%1\Intermediate
+tar -a -c -f SlateIconBrowser-%1.zip -C %temp%\SlateIconBrowser\%1\ *
+goto :eof
